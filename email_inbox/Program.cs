a65920fff9008752d876e6bf12f18b969196a1d0
jasaga2401 <jasaga@protonmail.com>
2023-12-04 10:14:01 +0000

    Add project files.

---
+++ email_inbox/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Mail;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+
+class Program
+{
+    static void Main()
+    {
+        string email = "<username>";
+        string password = "<usual password>";
+
+        using (var client = new ImapClient())
+        {
+            client.Connect("imap.gmail.com", 993, true);
+            client.Authenticate(email, password);
+
+            var inbox = client.Inbox;
+            inbox.Open(FolderAccess.ReadOnly);
+
+            var uids = inbox.Search(SearchQuery.All);
+
+            foreach (var uid in uids)
+            {
+                var message = inbox.GetMessage(uid);
+                Console.WriteLine($"Subject: {message.Subject}");
+                Console.WriteLine($"From: {message.From}");
+                Console.WriteLine($"Body: {message.TextBody}");
+                Console.WriteLine();
+            }
+
+            client.Disconnect(true);
+        }
+    }
+}
